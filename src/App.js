396c81f443954189690cae64063a4a9911f83f92
James <jamesvo801@gmail.com>
2021-04-24 12:15:26 -0700

    creating the repository

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Charts from './components/Charts';
+
+function App() {
+  return (
+    <div className="App">
+      <Charts></Charts>
+      <header className="App-header">
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
