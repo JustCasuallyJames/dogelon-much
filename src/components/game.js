05204cecb0a5a7f3be753e718221ecae21b8bf8b
ElvisLin125 <elvis.125.ma@gmail.com>
2021-04-25 02:36:52 -0700

    Fixed DogeCoin button not clicking, UI

--- src/components/game.js
+++ src/components/game.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import '../styles/game.scss';
 import '../styles/infoBar.scss';
+
+//graphics
 import HappyElon from '../styles/graphics/happy_Elon.png';
 import SadElon from '../styles/graphics/sad_Elon.png';
 import HappyElonUp from '../styles/graphics/happy_Elon up.png'
 import {ReactComponent as DogeClicker} from '../styles/graphics/doge-click.svg';
 import { ReactComponent as DogeCoin } from '../styles/graphics/dogecoin.svg';
-import Rules from './Rules';
 
+//souds
+//import useSound from 'use-sound';
+//import Owa from '../styles/sounds/owa.mp3';
+//import Bonk from '../styles/sounds/bonk.mp3';
+//import Wow from '../styles/sounds/wow.mp3';
 
-const maxes = [1,1,1,1,1,1,1,1,10,10];
+const maxes = [1,1,1,1,1,1,1,1,10,10]; 
 const generateRandom = () => {
     var max = 1;
     max = maxes[Math.floor(Math.random() * maxes.length)];
-    console.log("max: " + max);
     var min = 0.01;
-    return (Math.random() * (max-min) + min).toFixed(2);
+    var random = (Math.random() * (max-min) + min).toFixed(2)
+    console.log(random);
+    return random;
 } 
 
 class Game extends React.Component {
@@ -30,10 +37,12 @@ class Game extends React.Component {
             price : 0.01,
             profit: 0.01,
             totalCost: 0.00,
-            gameOpacity: 0,
-            menuOpacity: 1,
+            again: "",
             buyEnable: false,
-            buyAmount: 5
+            playAgain: false,
+            buyAmount: 5,
+            crashed: "",
+            
         }
         this.animateMusk = this.animateMusk.bind(this);
         this.start = this.start.bind(this);
@@ -58,6 +67,7 @@ class Game extends React.Component {
         }
     }
 
+
     buyCoin() {
         if (this.state.buyEnable == true) {
             var cost = Number(this.state.price) * Number(this.state.buyAmount);
@@ -65,23 +75,36 @@ class Game extends React.Component {
                 var newBalance = +(this.state.balance - cost).toFixed(2);
                 var newTotalCost = this.state.totalCost + cost;
                 this.setState({dogeCoins: this.state.dogeCoins+this.state.buyAmount, balance: newBalance, totalCost: newTotalCost})
-            }        
+            } 
         }
     }
 
     handleCrash() {
         if (this.state.price == this.state.limitValue) {
             clearInterval(this.interval);
-            this.setState({buyEnable: false, elon: SadElon})
-
+            this.setState({buyEnable: false, elon: SadElon, crashed: "CRASHED!"})
         }
     }
 
     start() {
-        this.setState({gameOpacity: 1, menuOpacity: 0});
+        clearInterval(this.interval);
+        if (this.state.playAgain == true) {
+            this.setState(
+                {
+                    limitValue : generateRandom(),
+                    dogeCoins: 5,
+                    price : 0.01,
+                    profit: 0.01,
+                    totalCost: 0.00,
+                    buyEnable: true,
+                    elon: HappyElon
+                }
+            )
+        }
+        this.setState({again: "again"});
         this.interval = setInterval(() => {
             var newPrice = +(this.state.price + 0.01).toFixed(3);
-            this.setState({price: newPrice, buyEnable: true});
+            this.setState({price: newPrice, buyEnable: true, playAgain: true});
             this.animateMusk();
             this.handleCrash();
             this.calculateProfit();
@@ -129,10 +152,9 @@ class Game extends React.Component {
                     <div id="cash-balance">$ {this.state.balance}</div>
                 </div>
 
-                <div id="game-container" style={{opacity: this.state.gameOpacity}}>
+                <div id="game-container">
                     <div id="stats">
-                        {/*<p id="doge-price">crash (developer): $ {this.state.limitValue}</p>*/}
-                        <p id="doge-price">Price: $ {this.state.price}</p>
+                        <p id="doge-price">Price: $ {this.state.price} <nobr id="crashed">{this.state.crashed}</nobr> </p>
                         <p id="doge-price">Dogecoins: {this.state.dogeCoins}</p>
                         <p id="doge-price">Profit: {this.state.profit}</p>
                     </div>
@@ -147,21 +169,12 @@ class Game extends React.Component {
                             Sell All
                         </button>
                         <button class="btn btn-info btn-lg"
-                            id="play-again-btn" onClick={this.playAgain}>
-                            Play Again
+                            id="play-again-btn" onClick={this.start}>
+                            Play {this.state.again}
                         </button>
                     </div>
                 </div>
 
-                <div id="start-menu" style={{opacity: this.state.menuOpacity}}>
-                    <Rules></Rules>
-                    <div id="start-btn-container">
-                        <button type="button" class="btn btn-primary btn-lg btn-info"
-                            onClick={this.start} id="start-btn" onClick={this.start}>
-                            START
-                        </button>
-                    </div>
-                </div>    
             </div>
         )
     }
